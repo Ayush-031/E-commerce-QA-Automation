9de3cb5f9dd17f7373bb7dba5fab228eae1b6ffa
Ayush Pandey <ayushpandey.31oct@gmail.com>
2026-08-28 14:53:01 +0530

    Add data-driven login testing

--- src/test/java/com/qa/ecommerce/tests/LoginTest.java
+++ src/test/java/com/qa/ecommerce/tests/LoginTest.java
@@ -4,24 +4,28 @@ import com.qa.ecommerce.base.BaseTest;
 import com.qa.ecommerce.pages.LoginPage;
 import com.qa.ecommerce.pages.ProductsPage;
 import com.qa.ecommerce.utils.ConfigReader;
+import com.qa.ecommerce.utils.TestDataReader;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
 
 public class LoginTest extends BaseTest {
 
-    @Test(groups = {"smoke", "regression"})
-    public void verifyValidLogin() {
+   @Test(
+    groups = {"smoke", "regression"},
+    dataProvider = "loginData"
+)
+public void verifyValidLogin(String username, String password) {
 
         LoginPage loginPage = new LoginPage(driver);
 
         loginPage.open();
 
-        ProductsPage productsPage = loginPage.login(
-                ConfigReader.get("username"),
-                ConfigReader.get("password")
+         ProductsPage productsPage = loginPage.login(
+                username,
+                password
         );
-
       Assert.assertEquals(
         productsPage.getProductsTitle(),
         "Products",
@@ -48,4 +52,22 @@ public class LoginTest extends BaseTest {
                 "Expected login error message was not displayed"
         );
     }
+    @DataProvider(name = "loginData")
+public Object[][] loginData() {
+
+    return new Object[][] {
+        {
+            TestDataReader.get("login.username.1"),
+            TestDataReader.get("login.password.1")
+        },
+        {
+            TestDataReader.get("login.username.2"),
+            TestDataReader.get("login.password.2")
+        },
+        {
+            TestDataReader.get("login.username.3"),
+            TestDataReader.get("login.password.3")
+        }
+    };
+}
 }
